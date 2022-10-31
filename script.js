fed3f961f223679d05947698fe42082b13577e2d
Debetome <magregory@tutanota.com>
2022-10-31 00:52:41 -0500

    Adding feature for saving the state in the local storage

--- script.js
+++ script.js
@@ -3,17 +3,74 @@ const copyBtn = document.getElementById("copy-password")
 const clearBtn = document.getElementById("clear")
 
 const passwordField = document.getElementById("password-field")
+const lengthField = document.querySelector(".length-field input")
 
+const charOptions = [...document.querySelectorAll(".check-options .check-option")]
+
+const LOCAL_STORAGE_KEY = "passwordGenerator.app"
+
+let stateData = {
+    charOptions: [true, false, true, false],
+    lengthFieldValue: 15,
+}
+
+function saveCurrentState() {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(stateData))
+}
+
+// Defining initial state
+function loadStateFromLocalStorage() {
+    const storedState = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (storedState) stateData = JSON.parse(storedState)
+    setCheckBoxesState()
+    setInputLengthState()
+}
+
+function setCheckBoxesState() {
+    for (let i=0; i<charOptions.length; i++) {
+        charOptions[i].querySelector("input").checked = stateData.charOptions[i]
+    }
+}
+
+function setInputLengthState() {
+    lengthField.value = stateData.lengthFieldValue
+}
+
+
+// Defining events
+function defineEvents() {
+    defineCheckBoxesEvents()
+    defineLengthInputEvent()
+}
+
+function defineCheckBoxesEvents() {
+    for (let i=0; i<charOptions.length; i++) {
+        charOptions[i].addEventListener("change", () => {
+            stateData.charOptions[i] = !stateData.charOptions[i]
+            saveCurrentState()
+        })
+    }
+}
+
+function defineLengthInputEvent() {
+    lengthField.addEventListener("change", () => {
+        stateData.lengthFieldValue = lengthField.value
+        saveCurrentState()
+    })
+}
+
+
+// Main logic
 function getChars() {
-    const options = [...document.querySelectorAll(".check-options .check-option")]
-    const checkedOptions = options.filter(option => option.querySelector("input").checked === true)
+    const charOptions = [...document.querySelectorAll(".check-options .check-option")]
+    const checkedOptions = charOptions.filter(option => option.querySelector("input").checked === true)
     const chars = checkedOptions.map(option => option.querySelector("label").innerText)
     return chars
 }
 
 function generatePassword() {
     const charOptions = getChars()
-    const length = document.querySelector(".length-field input").value
+    const length = lengthField.value
 
     if (charOptions.length === 0) return
     if (length === 0) return
@@ -41,6 +98,7 @@ function copyPassword() {
     })
 }
 
+// Initial instructions
 generateBtn.addEventListener("click", () => {
     generatePassword()
 })
@@ -53,3 +111,7 @@ clearBtn.addEventListener("click", () => {
     passwordField.value = ""
     console.log("Password cleared!")
 })
+
+loadStateFromLocalStorage()
+defineEvents()
+generatePassword()
